42fdf812194c3576d3556744e67264d7c9339b69
Farzaneh <fbokharaei1993@gmail.com>
2019-01-05 16:13:20 +0330

    add wallet

---
+++ app/src/main/java/com/example/malizen/bimarapp/Wallet.java
@@ -0,0 +1,20 @@
+package com.example.malizen.bimarapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Wallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+
+
+        Spinner spinner= (Spinner) findViewById(R.id.spinnerw);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.price,R.layout.spinnertxtwallet);
+        spinner.setAdapter(adapter);
+    }
+}
