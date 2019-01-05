502ab09222f7ac905e75ec88729f1cb9b4c0dd33
Leila <leilamuhammadi1079@gmail.com>
2019-01-05 14:56:10 +0330

    add SplashScreen

--- app/src/main/java/com/example/malizen/bimarapp/Splash.java
+++ app/src/main/java/com/example/malizen/bimarapp/Splash.java
@@ -1,7 +1,14 @@
 package com.example.malizen.bimarapp;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        YoYo.with(Techniques.Wobble)
+                .duration(1000)
+                .repeat(5)
+                .playOn(findViewById(R.id.img));
+
+
+
+        TextView tv = (TextView)findViewById(R.id.txt1);
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/IRANSansWeb(FaNum).ttf");
+        tv.setTypeface(tf);
+        TextView tv1 = (TextView)findViewById(R.id.txt2);
+        Typeface tf1 = Typeface.createFromAsset(getAssets(),"fonts/IRANSansWeb(FaNum).ttf");
+        tv.setTypeface(tf);
+        TextView tn2 = (TextView)findViewById(R.id.txt3);
+        Typeface tf2 = Typeface.createFromAsset(getAssets(),"fonts/IRANSansWeb(FaNum).ttf");
+        tv.setTypeface(tf);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                Intent i = new Intent(Splash.this,Introduction.class);
+//                startActivity(i);
+                finish();
+
+            }
+        },3000);
     }
 }
