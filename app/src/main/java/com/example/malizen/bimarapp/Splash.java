a898e0b2c47c71c77df7f60321a0383c26ec87f8
Farzaneh <fbokharaei1993@gmail.com>
2019-01-14 11:19:19 +0330

    Add SplashScreen

--- app/src/main/java/com/example/malizen/bimarapp/Splash.java
+++ app/src/main/java/com/example/malizen/bimarapp/Splash.java
@@ -1,14 +1,33 @@
 package com.example.malizen.bimarapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class Splash extends AppCompatActivity {
-
+    TextView txt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        YoYo.with(Techniques.Wobble)
+                .duration(1000)
+                .repeat(5)
+                .playOn(findViewById(R.id.img));
+        ;
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,Introduction.class);
+                startActivity(i);
+                finish();
+
+            }
+        },3000);
     }
 }
